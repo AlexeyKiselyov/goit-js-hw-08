3ba053976cd9ef0abebbf124c4a7237939cbd7f1
AlexeyKiselyov <aleshakiselev123@gmail.com>
2022-09-02 17:32:33 +0300

    Add 3d task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+// ------------------------------------
+const form = document.querySelector('.feedback-form');
+const inputEmail = document.querySelector('[name="email"]');
+const textarea = document.querySelector('[name="message"]');
 
+form.addEventListener('input', onFormListener);
+form.addEventListener('submit', onFormSubmit);
+
+const FEEDBACK_FROM_STATE = 'feedback-form-state';
+const textObj = { email: '', message: '' };
+
+const getFromLocStore = JSON.parse(
+  localStorage.getItem(FEEDBACK_FROM_STATE)
+) || { email: '', message: '' };
+
+inputEmail.value = getFromLocStore.email;
+textarea.value = getFromLocStore.message;
+
+function onFormListener(e) {
+  const {
+    elements: { email, message },
+  } = e.currentTarget;
+  textObj.email = email.value;
+  textObj.message = message.value;
+  const textObjToString = JSON.stringify(textObj);
+  localStorage.setItem(FEEDBACK_FROM_STATE, textObjToString);
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  localStorage.removeItem(FEEDBACK_FROM_STATE);
+  e.currentTarget.reset();
+}
