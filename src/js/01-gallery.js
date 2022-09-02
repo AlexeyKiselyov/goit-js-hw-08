b8647fa492d68730978a87580d9a970e6628a25a
AlexeyKiselyov <aleshakiselev123@gmail.com>
2022-09-02 13:17:27 +0300

    Add 1st and 2d tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
+// ----------- SimpleLightboxLibrary---------------
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// ------------------------------------------------
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector('.gallery');
+
+function createGalalery(arr) {
+  const galleryStr = arr
+    .map(({ preview, original, description }) => {
+      return `  
+    <a class="gallery__item" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"        
+        alt="${description}"        
+      />
+    </a>
+`;
+    })
+    .join('');
+  galleryBox.insertAdjacentHTML('beforeend', galleryStr);
+}
+
+createGalalery(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+// ----------Pre-existing player--------
+const frame = document.querySelector('iframe');
+const player = new Player(frame);
+// -------------------------------------
+const CURRENT_TIME = 'videoplayer-current-time';
+const getsCurrentTime = localStorage.getItem(CURRENT_TIME);
 
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    const time = data.seconds;
+    localStorage.setItem(CURRENT_TIME, time);
+  }, 1000)
+);
+
+player
+  .setCurrentTime(getsCurrentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        console.log(
+          'Error: the time was less than 0 or greater than the video’s duration'
+        );
+        break;
+
+      default:
+        console.log('Error: some other error occurred');
+        break;
+    }
+  });
